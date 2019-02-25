935fce5fb63f8a828104758d08cccdf04c461f13
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-02-25 20:09:12 +0100

    Add scene management

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("MainScene", LoadSceneMode.Single);
+    }
+}
