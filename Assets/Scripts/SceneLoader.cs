3b89b036de1fd0dba4d9a05923b69abe03b99a15
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-02-26 11:23:26 +0100

    Add comments and file info

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,5 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿// 'SceneLoader.cs' is part of AlienRunner-vTschopp
+// and licensed under the BSD 2-Clause License.
+//
+// For more information visit:
+// https://github.com/NotMyTschopp/AlienRunner-vTschopp
+//
+//
+// This script manages the loading of levels and quitting of the application.
+// Use as click function on buttons.
+
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
