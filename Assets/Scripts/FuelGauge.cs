496491ed201e4b43d5540832f88c38f8ea15bd4f
NotMyTschopp <45820353+NotMyTschopp@users.noreply.github.com>
2019-02-26 11:23:57 +0100

    Add fuel gauge

---
+++ Assets/Scripts/FuelGauge.cs
@@ -0,0 +1,51 @@
+﻿// 'FuelGauge.cs' is part of AlienRunner-vTschopp
+// and licensed under the BSD 2-Clause License.
+//
+// For more information visit:
+// https://github.com/NotMyTschopp/AlienRunner-vTschopp
+//
+//
+// This script manages the fuel of the UFO and the
+// fuel gauge drawn on the user interface.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FuelGauge : MonoBehaviour {
+
+    public Texture2D fuelIcon;
+
+    public GUIStyle fuelBarStyle = new GUIStyle();
+    public GUIStyle fuelBarStyleBackground = new GUIStyle();
+
+    private Vector2 position = new Vector2(20, 20);
+    private Vector2 size = new Vector2(200, 20);
+
+    private int fuelPercentage = new int();
+
+    private void drawFuelBar()
+    {
+        GUI.BeginGroup(new Rect(position.x, position.y, size.x, size.y));
+
+        // Background of fuel bar.
+        GUI.Box(new Rect(0, 0, size.x, size.y), "", fuelBarStyleBackground);
+
+        // Fuel bar
+        GUI.BeginGroup(new Rect(0, 0, size.x * (fuelPercentage / 100f), size.y));
+        GUI.Box(new Rect(0, 0, size.x * (fuelPercentage / 100f), size.y), fuelPercentage + "%", fuelBarStyle);
+        GUI.EndGroup();
+
+        GUI.EndGroup();
+    }
+
+    private void OnGUI()
+    {
+        drawFuelBar();
+    }
+
+    private void Start()
+    {
+        fuelPercentage = 100;
+    }
+}
